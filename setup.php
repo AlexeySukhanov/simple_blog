edd72d26b1071d1107fec18b5af94cc26258c7f5
Alexey Sukhanov <yellocoma@gmail.com>
2018-08-20 04:22:51 +0300

    create users table

--- setup.php
+++ setup.php
@@ -5,3 +5,11 @@
  * Date: 19.08.2018
  * Time: 4:40
  */
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try{
+    $db->query($queryStr);
+} catch(PDOException $e){
+    echo $e->getMessage();
+}
