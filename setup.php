736a0af82e94459a439b261d967f7ed12cb35783
Alexey Sukhanov <yellocoma@gmail.com>
2018-08-21 13:19:01 +0300

    setup.php ref

--- setup.php
+++ setup.php
@@ -6,20 +6,19 @@
  * Time: 4:40
  */
 
-$host = "127.0.0.1";
-$db = "simple_blog";
-$user = "admin";
-$pass = "0000";
+$host    = "127.0.0.1";
+$db      = "simple_blog";
 $charset = "utf8";
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
+$user    = "admin";
+$pass    = "0000";
+$opt     = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES   => false
 ];
 
-$pdo= new PDO( $dsn, $user, $pass, $opt);
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$pdo = new PDO( $dsn, $user, $pass, $opt );
 
 try{
     $queryStr = "CREATE TABLE users ( id INTEGER  NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40), password VARCHAR(100), email VARCHAR(150) )";
