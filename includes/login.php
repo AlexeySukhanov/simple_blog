a4dacd28380491d1d9e72059a70800e8cdaca931
Alexey Sukhanov <yellocoma@gmail.com>
2018-10-28 01:59:57 +0300

    Login->__construct(), Login->index()

--- includes/login.php
+++ includes/login.php
@@ -10,14 +10,24 @@ require_once('database.php');
 
 class Login
 {
+    public $simple_blog_db;
+    public $base;
     public function __construct()
     {
-
+        $this->simple_blog_db = new Database();
+        $this->base = new stdClass(); //(object)'';
+        $this->base->url = 'http://' . $_SERVER['SERVER_NAME'];
+        $this->index();
     }
 
     public function index()
     {
-
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $this->validateDetails();
+        } elseif(!empty($_GET['status']) && $_GET['status'] = 'inactive'){
+            $error = 'Сеанс завершен в связи с отсутствием активности. Пожалуйста, авторизируйтесь снова';
+        }
+        require_once 'admin/tmpl/loginform.php';
     }
 
     public function loginSuccess()
