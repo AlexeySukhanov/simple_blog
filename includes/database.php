76ccfbe2f9b9a933d3338c0b4940d79d1e802068
Alexey Sukhanov <yellocoma@gmail.com>
2018-11-01 02:49:38 +0300

    REFACTORING: $db to $db_name;

--- includes/database.php
+++ includes/database.php
@@ -8,7 +8,7 @@
 
 class Database {
     public $host    = "";
-    public $db      = "";
+    public $db_name  = "";
     public $charset = "";
     public $user    = "";
     public $pass    = "";
@@ -20,7 +20,7 @@ class Database {
     public function __construct()
     {
         $this->host    = "127.0.0.1";
-        $this->db      = "simple_blog";
+        $this->db_name  = "simple_blog";
         $this->charset = "utf8";
         $this->user    = "admin";
         $this->pass    = "0000";
@@ -30,7 +30,7 @@ class Database {
             PDO::ATTR_EMULATE_PREPARES   => false
         ];
 
-        $this->dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        $this->dsn = "mysql:host=$this->host;dbname=$this->db_name;charset=$this->charset";
         $this->pdo = new PDO( $this->dsn, $this->user, $this->pass, $this->opt );
     }
 
