7a8dca137bd0d07d415bfede63a90a93022c6270
Alexey Sukhanov <yellocoma@gmail.com>
2018-10-31 20:17:12 +0300

    add for admin header.php, footer.php

---
+++ admin/tmpl/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Site Name</title>
+    <link rel="stylesheet" href="frontend/ext/foundation/css/foundation.css">
+    <link rel="stylesheet" href="frontend/int/css/main.css">
+</head>
+<body>
+
+<br>
+<div class="grid-container">
+    <div class="grid-x grid-margin-y">
+        <div class="cell clearfix">
+            <a href="<?php echo $this->base->url . '/admin' ?>" class="button float-right hollow secondary">Admin login</a>
+        </div>
+    </div>
+
+
