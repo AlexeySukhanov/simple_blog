186adb549be144f68c9ec85281af09088e6b1458
Alexey Sukhanov <yellocoma@gmail.com>
2018-12-05 02:03:41 +0300

    REMOVE: debugging code

--- vendors/pagedown/pagedown.js
+++ vendors/pagedown/pagedown.js
@@ -1,23 +1,14 @@
-
-
 $(document).ready(function(){
     if($('div').is('#wmd-preview')){
-
         (function(){
-            //alert('lol');
             var converter = new Markdown.Converter();
             var editor = new Markdown.Editor(converter);
             editor.run();
         }());
 
-        alert('lol');
-
-
         $('#createEnty').submit(function(){
             var content = $('#wmd-preview').html();
             $('#hcontent').val(content);
-            alert('hcontent: ' + $('#hcontent').val());
         });
     }
-
 });
