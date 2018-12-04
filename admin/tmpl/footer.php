984f23254d89fa7266a23eaed3addc9afbe853ef
Alexey Sukhanov <yellocoma@gmail.com>
2018-12-04 18:59:12 +0300

    BUG: add closing body tag

--- admin/tmpl/footer.php
+++ admin/tmpl/footer.php
@@ -4,4 +4,4 @@
         <script src="/vendors/what-input/what-input.js"></script>    <!-- foundation dependencies -->
         <script src="/vendors/foundation/js/foundation.js"></script>
         <script src="/vendors/foundation/js/app.js"></script>        <!-- foundation initiation -->
-    </bvendors
+    </body>
