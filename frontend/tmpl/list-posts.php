4afa2c7ac30177665a9275088f2ee7ac32382854
Alexey Sukhanov <yellocoma@gmail.com>
2019-02-15 00:00:33 +0300

    ADD: comments count output

--- frontend/tmpl/list-posts.php
+++ frontend/tmpl/list-posts.php
@@ -6,6 +6,7 @@
         <div class="cell">
             <h2>Сообщение №<?= $post['id']; ?>: <?= htmlspecialchars($post['title']); ?>.</h2>
             <p><?= implode(' ',  array_slice(explode(' ', strip_tags(($post['content']))), 0, 40)); ?>[...]</p>
+            <p>Комментариев: <?= $post['comments']; // TODO: Сделать аналогичный вывод количества комментов в админке ?></p>
             <a href="<?= $this->base->url . '/?id=' . $post['id']; ?>" class="button hollow">Подробнее</a>
             <hr>
         </div>
