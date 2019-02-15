2ab374b07cfa1877a7dc78493eaf6fdc4920918b
Alexey Sukhanov <yellocoma@gmail.com>
2019-02-16 01:06:32 +0300

    ADD: comments output

--- frontend/tmpl/view-post.php
+++ frontend/tmpl/view-post.php
@@ -2,16 +2,27 @@
 
 <div class="grid-y grid-margin-y">
     <h1>View Post</h1>
-    <?php foreach( $posts as $post ): ?>
-        <div class="cell">
-            <h2>Сообщение №<?= $post['id']; ?>: <?= htmlspecialchars($post['title'], ENT_QUOTES); ?></h2>
-            <p><?= htmlspecialchars($post['content'], ENT_QUOTES ); ?></p>
-            <hr>
-        </div>
-    <?php endforeach; ?>
     <div class="cell">
         <a href="<?= $this->base->url; ?>" class="button hollow">Вернуться к списку сообщений</a>
     </div>
+    <?php foreach($posts as $post): ?>
+        <div class="cell">
+            <h2>Сообщение №<?= $post['id']; ?>: <?= htmlspecialchars($post['title'], ENT_QUOTES); ?></h2>
+            <p><?= $post['content']; ?></p>
+            <hr>
+        </div>
+    <?php endforeach; ?>
+
+    <?php if(!empty($post_comments)): ?>
+        <div class="h3">Комментарии:</div>
+    <?php endif; ?>
+    <?php foreach ($post_comments as $comment): ?>
+        <div class="cell">
+            <div class="h4"><?= $comment['name']; ?> (<a href="mailto:<?= $comment['email']; ?>"><?= $comment['email']; ?></a>)</div>
+            <p><?= $comment['comment']; ?></p>
+            <hr>
+        </div>
+    <?php endforeach; ?>
 </div>
 
 <?php require_once 'footer.php'; ?>
